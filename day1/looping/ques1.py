1a8760ce28d800c8c53bf3f27bfd11de9daf1d16
rushan bhandari <rushanbhandari@Rushan.local>
2026-04-14 21:00:19 +0545

    looping

---
+++ day1/looping/ques1.py
@@ -0,0 +1,8 @@
+#Write a program to print multiplication table of a given number using for loop Attempt problem 1 using while loop.
+
+number = int(input("Enter a number: "))
+i = 1
+while i <= 10:
+    print(f"{number} x {i} = {number * i}")
+    i += 1
+    
