a600b6fe6d9efe73f3b233c2f23ff74e4bddceb1
rushan bhandari <rushanbhandari@Rushan.local>
2026-04-14 20:51:54 +0545

    Conditional Expressions

---
+++ day1/conditional expressions/ques7.py
@@ -0,0 +1,8 @@
+#Write a program to find out whether a given post is talking about “Rushan” or not.
+
+post = input("Enter the post: ")
+
+if "Rushan" in post:
+    print("The post is talking about Rushan.")
+else:
+    print("The post is not talking about Rushan.")
