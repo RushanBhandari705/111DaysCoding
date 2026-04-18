fcc64e55dc8d27aa4dce2400f13683c843d1429e
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-04-18 20:21:21 +0545

    Update ques2.py

--- ques2.py
+++ ques2.py
@@ -1,4 +1,4 @@
-#Create a class ‘Pets’ from a class ‘Animals’ and further create a class ‘Dog’ from ‘Pets’. Add a method ‘bark’ to class ‘Dog’.
+
 class Animals:
     def __init__(self, name):
         self.name = name
