0efb6f90cfc3139f001ca4b9bb3c7c2040e86386
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-04-18 20:22:33 +0545

    Update ques8.py

--- ques8.py
+++ ques8.py
@@ -1,4 +1,4 @@
-#Override the __len__() method on vector of problem 5 to display the dimension of the vector.
+
 class Vector:
     def __init__(self, components):
         self.components = components
