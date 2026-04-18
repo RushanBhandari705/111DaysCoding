c06581254a35d8b3e8fe537e33801c3cce1eb47b
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-04-18 20:19:08 +0545

    Add files via upload

---
+++ ques4.py
@@ -0,0 +1,27 @@
+#Write a method ‘salaryAfterIncrement’ method with a @property decorator with a setter
+#which changes the value of increment based on the salary.
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self._salary = salary
+        self._increment = 0
+
+    @property
+    def salary(self):
+        return self._salary
+
+    @salary.setter
+    def salary(self, value):
+        self._salary = value
+        self._increment = value - self._salary
+
+    @property
+    def salaryAfterIncrement(self):
+        return self._salary + self._increment
+
+    @salaryAfterIncrement.setter
+    def salaryAfterIncrement(self, value):
+        self._increment = value - self._salary
+
+    def __str__(self):
+        return f"Employee: {self.name}, Salary: {self._salary}, Increment: {self._increment}"
