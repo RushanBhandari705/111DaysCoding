dccf8218ad3882e3625de155924fd41d12ec6ece
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-04-18 20:22:05 +0545

    Update ques6.py

--- ques6.py
+++ ques6.py
@@ -1,4 +1,4 @@
-#Write a class vector representing a vector of n dimensions. Overload the + and * operator which calculates the sum and the dot(.) product of them.
+
 class Vector:
     def __init__(self, components):
         self.components = components
